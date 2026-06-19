af8cb55973aa657bb31db8305f8405799ed46717
ardijancuri <ardijancuri3@hotmail.com>
2026-06-19 20:23:48 +0200

    fix: add suppressHydrationWarning to body element for improved hydration handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,6 +79,7 @@ export default function RootLayout({
       </head>
       <body
         className={`${spaceGrotesk.variable} ${jetbrainsMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         {children}
       </body>
