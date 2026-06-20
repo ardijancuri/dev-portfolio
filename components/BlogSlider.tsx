8aa75e91a0e47691f16ecb96633a7a5741e301ef
ardijancuri <ardijancuri3@hotmail.com>
2026-06-20 17:34:44 +0200

    Limit blog slider to three posts

--- components/BlogSlider.tsx
+++ components/BlogSlider.tsx
@@ -2,7 +2,7 @@ import { getBlogPosts } from "@/lib/blog";
 import BlogSliderTrack from "@/components/BlogSliderTrack";
 
 export default async function BlogSlider() {
-  const posts = await getBlogPosts(4);
+  const posts = await getBlogPosts(3);
 
   return <BlogSliderTrack posts={posts} />;
 }
--- components/BlogSliderTrack.tsx
+++ components/BlogSliderTrack.tsx
@@ -15,7 +15,7 @@ export default function BlogSliderTrack({
   }
 
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
       {posts.map((post) => (
         <BlogPostCard key={post.id} post={post} variant="home" />
       ))}
